28715a92190238cb9fdfee110a0a67fcb2254a8e
lgnq <dzzxzz@gmail.com>
2017-03-29 02:42:31 +0000

    del space

--- main.py
+++ main.py
@@ -432,14 +432,14 @@ if __name__ == '__main__':
         
             if len(cmd_list) == 1:
                 winno = input("winno = ")
-                img_spi_addr  = input("address = ")
+                img_spi_addr = input("address = ")
             elif len(cmd_list) == 2:
                 winno = string.atoi(cmd_list[1], 16)
                 print 'winno = ' + cmd_list[1]
-                img_spi_addr  = input("address = ")
+                img_spi_addr = input("address = ")
             elif len(cmd_list) == 3:
                 winno = string.atoi(cmd_list[1], 16)
-                img_spi_addr  = string.atoi(cmd_list[2], 16)
+                img_spi_addr = string.atoi(cmd_list[2], 16)
                 
             print hex(img_spi_addr)
         
